9558b5d38514b3653fb940ae530c8bb04f3420c7
Sam Figueroa <unimatrixzxero+gravatar@gmail.com>
2012-08-03 15:25:40 +0200

    Add ability to toggle rows in time reports

--- dedirm01.fuchs-gewuerze.local.js
+++ dedirm01.fuchs-gewuerze.local.js
@@ -1,9 +1,3 @@
-var my_name = 'Sam Figueroa'
-$('#time-report tbody td').each(function() {
-  if (this.innerHTML.match(my_name)) {
-    console.log(this.innerHTML);
-    $(this).parent().css({
-      background: 'yellow'
-    });
-  }
+$('#time-report tbody tr').click(function() {
+  $(this).toggleClass('total');
 });

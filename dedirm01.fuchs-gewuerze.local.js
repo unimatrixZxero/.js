73288b93627b8b706de42df6b057c913f937528e
Sam Figueroa <unimatrixzxero+gravatar@gmail.com>
2012-08-06 14:49:55 +0200

    Re-add the auto hightlight of my own name on reload

--- dedirm01.fuchs-gewuerze.local.js
+++ dedirm01.fuchs-gewuerze.local.js
@@ -1,3 +1,13 @@
 $('#time-report tbody tr').click(function() {
   $(this).toggleClass('total');
 });
+
+var my_name = 'Sam Figueroa'
+
+$('#time-report tbody td').each(function() {
+  if (this.innerHTML.match(my_name)) {
+    $(this).parent().css({
+      background: 'yellow'
+    });
+  }
+});

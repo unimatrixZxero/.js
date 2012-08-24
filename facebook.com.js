ac2251c277e5355de46d41b5cec329abd7c7a19d
Sam Figueroa <unimatrixzxero@gmail.com>
2012-08-24 17:46:56 +0200

    Make facebook header less obtrusive and hide ads.

---
+++ facebook.com.js
@@ -0,0 +1,11 @@
+$('#blueBar').css('background-color','#2D2D2D');
+
+document.getElementById('pageLogo').innerHTML = '4 TEH LULz';
+
+
+function hide_adds(){
+  $('.UIStandardFrame_SidebarAds').hide();
+  $('.ego_section').hide();
+}
+
+setInterval(function() {hide_adds()}, 1000);

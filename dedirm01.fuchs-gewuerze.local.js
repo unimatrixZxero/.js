ac8f1b15a5cb02fcf38eaef220fef596e413d70f
Sam Figueroa <unimatrixzxero@gmail.com>
2012-09-03 12:12:28 +0200

    Update documentation for dedirm01

--- dedirm01.fuchs-gewuerze.local.js
+++ dedirm01.fuchs-gewuerze.local.js
@@ -2,8 +2,9 @@ $('#time-report tbody tr').click(function() {
   $(this).toggleClass('total');
 });
 
-var my_name = 'Sam Figueroa'
 
+// Highlight the row for my name in reports
+var my_name = 'Sam Figueroa'
 $('#time-report tbody td').each(function() {
   if (this.innerHTML.match(my_name)) {
     $(this).parent().css({
@@ -14,13 +15,16 @@ $('#time-report tbody td').each(function() {
 
 $('body').css('font-family', 'Helvetica Neue')
 
+// Ooh, magenta baby!
 $('#header').css('background-color','#C6015D')
 
 
+// focus on first field automatically
 if ($('#time_entry_issue_id').length > 0) {
   $('#time_entry_issue_id').focus();
 }
 
+// Set default activity to development
 if($('#time_entry_activity_id').length > 0) {
   $('#time_entry_activity_id').val(9)
 }

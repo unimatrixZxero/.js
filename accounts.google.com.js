36ba581ef6bd08a5b106feb4f34ee00c83e1c4b9
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2012-10-10 21:56:08 +0200

    Added comments, further nicefied table

--- accounts.google.com.js
+++ accounts.google.com.js
@@ -1,17 +1,26 @@
 $(function(){
 
-	if(document.location.pathname == '/b/0/IssuedAuthSubTokens'){
+	if(document.location.pathname.match(/IssuedAuthSubTokens/)){
 		$('head').append($('<link/>').attr({href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.1.1/css/bootstrap.css', rel: 'stylesheet'}));
-		$('#acList').addClass('table table-hover table-striped');
+		
+		// Nicefy table
+		$('#acList').addClass('table table-hover table-striped table-condensed');
+		$('a', '#acList').addClass('btn btn-danger');
+		var td_html = $('a', '#acList').closest('td').html();
+		$('a', '#acList').closest('td').html(td_html.replace(/\[|\]/g, ''));
+
+		// Hide and toggle list of connected services
 		$('ul').hide();
-		var showHideToggle = $('<a/>').addClass('btn btn-success').text('Toggle list');
-		showHideToggle.appendTo($('h3').first());
+		var showHideToggle = $('<a/>').addClass('btn').text('Toggle list');
+		showHideToggle.appendTo($('h3').first().next());
 		showHideToggle.click(function(){
 			$('ul').first().toggle();
 		});
 
+		// Make page nicer
 		$('input[type=submit]').addClass('btn');
 
+		// I no longer need an explanation what I'm doing! Let's get rid of it.
 		$('hr').first().nextUntil('fieldset').remove();
 		$('fieldset').css('margin-top', '-20px');
 		$('.body').css('color', 'white');

b2c44dc80ae97268d47105f349f132a6490426ae
Sam Figueroa <unimatrixzxero@gmail.com>
2012-11-09 13:42:32 +0100

    Darken titlebar for facebook
    - get rid of the blue

--- facebook.com.js
+++ facebook.com.js
@@ -2,6 +2,10 @@ $('#blueBar').css('background-color','#2D2D2D');
 
 $('body').css('font-family', 'Helvetica Neue');
 
+// Grey it out
+$('#contentCol, body').css('background-color', '#E4E4E4');
+
+
 document.getElementById('pageLogo').innerHTML = '4 TEH LULz';
 
 

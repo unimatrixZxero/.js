e49ef17fb0e17399c23e47509ca6e88bff285483
Sam Figueroa <unimatrixzxero@gmail.com>
2012-11-09 13:42:05 +0100

    Switch font and slow intervall checking for ads

--- facebook.com.js
+++ facebook.com.js
@@ -1,5 +1,7 @@
 $('#blueBar').css('background-color','#2D2D2D');
 
+$('body').css('font-family', 'Helvetica Neue');
+
 document.getElementById('pageLogo').innerHTML = '4 TEH LULz';
 
 
@@ -7,5 +9,4 @@ function hide_adds(){
   $('.UIStandardFrame_SidebarAds').hide();
   $('.ego_section').hide();
 }
-
-setInterval(function() {hide_adds()}, 1000);
+setInterval(function() {hide_adds()}, 3000);

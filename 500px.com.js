aeab8f3c114dcc8da2ce3209705ab1e9efb70855
Sam Figueroa <unimatrixzxero@gmail.com>
2014-01-20 14:46:48 +0100

    Update script against latest counter measures

--- 500px.com.js
+++ 500px.com.js
@@ -1,4 +1,12 @@
 // the sex is on fire
 $(window).ready(function(){
-  $('body').append($('#thephoto a img').clone());
+  var img = $('img.the_photo')[0];
+  var img_link = document.createElement('a');
+  img_link.innerHTML = "Open image in new tab";
+  img_link.target = 'new_tab';
+  img_link.href = img.src;
+  img_link.id = 'img_link'
+  $(img_link).css({padding: '10px', backgroundColor: 'grey', color: 'white', margin: '10px', position: 'absolute', top: 0, left: 0, width: '50px', zIndex: '1010101'});
+  $('body').append(img_link)
+  $('#img_link').trigger('click');
 });

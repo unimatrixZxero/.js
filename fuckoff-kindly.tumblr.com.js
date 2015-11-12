02bcf54fa2b724a462ed075ce1bb3d93aae268a5
Sam Figueroa <github@samuelfigueroa.com>
2015-11-12 10:23:32 +0100

    Remove custom script for fuckoff kindly
    - Tumblr page no longer exists

--- fuckoff-kindly.tumblr.com.js
+++
@@ -1,8 +0,0 @@
-console.log('Script fuckoff-kindly loaded.');
-$(document).ready(function(){	
-	setTimeout(function() {
-		document.ondragstart = function(){return true;}
-		$('.box a')[0].href  = $('div.box a img')[0].src		
-	}, 1000);	
-})
-
